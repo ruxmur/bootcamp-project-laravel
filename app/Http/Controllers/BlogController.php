dd77a4883c71a5518d75532f8743bb81d366d167
ruxmur <r.murtacaeva@developmentaid.it>
2021-12-22 20:51:52 +0200

    creat controllers fot my database

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use App\Models\BlogCategory;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-  public function index() {
-      $articles = Article::orderby('created_at', 'DESC')->get()->all();
-      return view('blog.blog', ['articles' => $articles]);
+  public function blog()
+  {
+    $request = request()->all();
+    $sort = $request['sort'] ?? 'ASC';
+
+    $categories = BlogCategory::all();
+    $category = $request['category'] ?? $categories->first()->id;
+
+    $articles = Article::orderby('created_at', $sort)->paginate(7);
+    $articles->appends(['sort' => $sort]);
+
+    
+    // $articles = Article::orderby('created_at', 'DESC')->get()->all();
+    return view('blog.blog', [
+      'articles' => $articles,
+      'categories' => $categories,
+      'filter' => [
+        'sort' => $sort,
+      'category' => (int)$category,
+      ]
+    ]);
   }
 }
