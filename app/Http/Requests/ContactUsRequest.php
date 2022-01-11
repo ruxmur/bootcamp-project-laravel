456801eaed2c8793129e5e7a0069f759e6c29a74
ruxmur <r.murtacaeva@developmentaid.it>
2022-01-11 22:08:17 +0200

    Add initial route for email sending and validation for provided data.

---
+++ app/Http/Requests/ContactUsRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ContactUsRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'firstName' => ['min:2', 'required', 'string'],
+            'lastName' => ['min:2', 'required', 'string'],
+            'email' => ['string', 'email', 'required'],
+            'tel' => ['string', 'tel', 'required'],
+            'sex' => ['required', 'string', Rule::in(['female', 'male'])],
+            'membership' => ['required', 'string', Rule::in(['Trial - 1 month', 'Standart - 12 month', 'Premium - 36 month'])],
+            'sport' => ['required', 'string', Rule::in(['Aerobic exercise', 'Box', 'Cardio', 'CrossFit', 'Stretching', 'Strenght training', 'Yoga'])],
+            'coach'  => ['required', 'string', Rule::in(['Alex', 'Kevin', 'Lana', 'Mark', 'Rockwell', 'Ruth', 'Tom'])],
+            'message' => ['required', 'string', 'min:10'],
+
+            // 'readTerms' => ['required']
+        ];
+
+    }
+}
--- resources/views/contacts/contactUs.blade.php
+++ resources/views/contacts/contactUs.blade.php
@@ -1,106 +1,241 @@
 @extends('layout')
 @section('content')
-<section class="contact bg_grey" >
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-md-12 banner_img text-white">
-          <div class="col-md-6 img_opacity">
-            <img src="./images/We Are Just a Step Closer.png" alt="img" class="img-fluid img_1" />
-            <img src="./images/content.png" alt="img" class="img-fluid img_2" />
-          </div>
-        </div>
-      </div>
-
-      <div class="col-md-2 ml-auto rectangle_1"></div>
-    </div>
-  </section>
-
-  <section class="contact_us bg_contact">
-    <div class="container bg_light bg-white">
-      <div class="row">
-        <div class="col-md-5">
-          <div class="col-md-1 top_square bg-dark"></div>
-          <h4 class="mb-3">
-            Contact Us
-          </h4>
-          <h5 class="hide_1">Find Us and Connect</h5>
-          <h5 class="know_more text-center">
-            Know More About Our History
-          </h5>
-          @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            @if (session()->has('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
-          <form action="{{ route('contactUs.send') }}" method="POST" name="contact-form" >
-            {{ csrf_field() }}   
-            <!-- @csrf -->
-            <div class="form-row">
-              <div class="form-group col-md-12">
-                <input name="name" type="text" value="{{ old('name') }}" class="form-control form-control-lg mt-3 mb-2" placeholder="Name" />
-              </div>
-              <div class="form-group col-md-12">
-                <input name="email" type="email" value="{{ old('email') }}" class="form-control form-control-lg mb-2" placeholder="E-mail Address" />
-              </div>
-              <div class="form-group col-sm-12">
-                <textarea name="message" class="form-control form-control-lg mb-2" placeholder="Message" rows="6"></textarea>
-              </div>
-              <div class="form-group col-md-12">
-                <input type="submit" class="btn btn-primary btn-block mb-3" value="Send Message" />
-              </div>
-            </div>
-          </form>
-        </div>
-
-        <div class="col-md-5 ml-auto text-white pt- pb-4 bg_dark">
-          <div class="pt-5 px-4">
-            <img src="./images/email.png" alt="img" class="img-fluid" />
-            <p class="ml-5">joinus@power.com</p>
-          </div>
-
-          <div class="pt-5 px-4">
-            <img src="./images/map.png" alt="img" class="img-fluid" />
-            <p class="ml-5">125/3 S.Lincoln St., Washington DC </p>
-          </div>
-
-          <div class="pt-5 px-4">
-            <img src="./images/phone.png" alt="img" class="img-fluid" />
-            <p class="ml-5">202.458.6016</p>
-          </div>
-
-          <div class="pt-5 px-4">
-            <img src="./images/wall-clock.png" alt="img" class="img-fluid" />
-            <p class="ml-5">Weekdays Monday - Friday 07:00 - 21:00</p>
-            <p class="ml-5">Weekdays Saturday - Sunday 09:00 - 19:00</p>
-          </div>
-        </div>
-
-        <div class="col-md-2 right_block"></div>
-      </div>
-    </div>
-  </section>
-
-  <div class="container-fluid padding py-5">
+<section class="contact bg_grey ">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-12">
-        <p class="google__maps">
-          <iframe id="maps"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49585.36384389782!2d-76.96125934403132!3d39.03617588340584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7c375724da991%3A0x675f88e616c0377b!2sBeltsville%2C%20MD%2C%20USA!5e0!3m2!1sen!2sng!4v1593515416673!5m2!1sen!2sng"
-            width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-            tabindex="0"></iframe>
-        </p>
+      <div class="col-md-12 banner_img text-white">
+        <div class="col-md-8 img_opacity">
+        </div>
       </div>
     </div>
   </div>
+</section>
+<div>
+  <h4><i class="fa fa-calendar"></i>Make Reservations in Minutes</h4>
+</div>
+<div class="row">
+  <div class="col-md-4 order-md-2 mb-4">
+    <h4 class="d-flex justify-content-between align-items-center mb-3">
+      <span class="text-muted">Your cart</span>
+      <span class="badge badge-secondary badge-pill">3</span>
+    </h4>
+    <ul class="list-group mb-3">
+      <li class="list-group-item d-flex justify-content-between lh-condensed">
+        <div>
+          <h6 class="my-0">Product name</h6>
+          <small class="text-muted">Brief description</small>
+        </div>
+        <span class="text-muted">$100</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between lh-condensed">
+        <div>
+          <h6 class="my-0">Second product</h6>
+          <small class="text-muted">Brief description</small>
+        </div>
+        <span class="text-muted">$800</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between lh-condensed">
+        <div>
+          <h6 class="my-0">Third item</h6>
+          <small class="text-muted">Brief description</small>
+        </div>
+        <span class="text-muted">$3.500</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between bg-light">
+        <div class="text-success">
+          <h6 class="my-0">Promo code</h6>
+          <small>EXAMPLECODE</small>
+        </div>
+        <span class="text-success">$25%</span>
+      </li>
+      <li class="list-group-item d-flex justify-content-between">
+        <span>Total (USD)</span>
+        <strong>$20</strong>
+      </li>
+    </ul>
+    @if ($errors->any())
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+    @endif
+    @if (session()->has('success'))
+    <div class="alert alert-success">
+      {{ session('success') }}
+    </div>
+    @endif
+    <form action="{{ route('contactUs.send') }}" method="POST" name="contact-form" class="card p-2">
+      <!-- {{ csrf_field() }} -->
+      <div class="input-group">
+        <input name="promoCode" type="text" value="{{ old('promo code') }}" class="form-control " placeholder="Promo code">
+        <div class="input-group-append">
+          <button type="submit" class="btn btn-secondary">Redeem</button>
+        </div>
+      </div>
+    </form>
+
+  </div>
+  <div class="col-md-8 order-md-1">
+    <h4 class="mb-3">Billing address</h4>
+    <form class="needs-validation" novalidate>
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <label for="firstName">First name</label>
+          <input name="firstName" type="text" value="{{ old('first name') }}" class="form-control" id="firstName" placeholder="" required>
+          <div class="invalid-feedback">
+            Valid first name is required.
+          </div>
+        </div>
+        <div class="col-md-6 mb-3">
+          <label for="lastName">Last name</label>
+          <input name="lastName" type="text" value="{{ old('last name') }}" class="form-control" id="lastName" placeholder="" required>
+          <div class="invalid-feedback">
+            Valid last name is required.
+          </div>
+        </div>
+      </div>
+
+      <div class="mb-3">
+        <label for="email">Email</label>
+        <input name="email" type="email" value="{{ old('email') }}" class="form-control" id="email" placeholder="you@example.com">
+        <div class="invalid-feedback">
+          Please enter a valid email address for shipping updates.
+        </div>
+      </div>
+
+      
+      <div class="mb-3">
+        <label for="telNo">Telephone / Mobile</label>
+        <input name="telNo" type="tel" value="{{ old('telNo') }}" class="form-control" id="tel" placeholder="+(1) 000 000 000">
+      </div>
+
+      <div>
+        <div class="col-md-13 mb-3">
+          <label for="sex">Sex</label>
+          <select class="custom-select d-block w-100" id="sex" required>
+            <option value="">Choose...</option>
+            <option>Female</option>
+            <option>Male</option>
+          </select>
+          </div>
+      </div>
+
+      <div>
+        <div class="col-md-13 mb-3">
+          <label for="membership">Membership</label>
+          <select class="custom-select d-block w-100" id="membership" required>
+            <option value="">Choose...</option>
+            <option>Trial - 1 month</option>
+            <option>Standart - 12 month</option>
+            <option>Premium - 36 month</option>
+          </select>
+          <div class="invalid-feedback">
+            Please select a valid type of membership.
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <label for="sport">Sport</label>
+          <select class="custom-select d-block w-100" id="sport" required>
+            <option value="">Choose...</option>
+            <option>Aerobic exercise</option>
+            <option>Box</option>
+            <option>Cardio</option>
+            <option>CrossFit</option>
+            <option>Stretching</option>
+            <option>Strenght training</option>
+            <option>Yoga</option>
+          </select>
+          <div class="invalid-feedback">
+            Please provide a valid type of sport.
+          </div>
+        </div>
+        <div class="col-md-6 mb-3">
+          <label for="coach">Coach</label>
+          <select class="custom-select d-block w-100" id="coach" required>
+            <option value="">Choose...</option>
+            <option>Alex</option>
+            <option>Kevin</option>
+            <option>Lana</option>
+            <option>Mark</option>
+            <option>Rockwell</option>
+            <option>Ruth</option>
+            <option>Tom</option>
+          </select>
+          <div class="invalid-feedback">
+            Please provide a valid coach.
+          </div>
+        </div>
+      </div>
+      <div class="form-group col-sm-13">
+        <textarea name="message" class="form-control form-control-lg mb-2" placeholder="Message" rows="6"></textarea>
+      </div>
+      <hr class="mb-4">
+
+      <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="save-info">
+        <label class="custom-control-label" for="save-info">Save this information for next time</label>
+      </div>
+      <hr class="mb-4">
+
+      <h4 class="mb-3">Payment</h4>
+
+      <div class="d-block my-3">
+        <div class="custom-control custom-radio">
+          <input id="credit" name="paymentMethod" type="radio" class="custom-control-input" checked required>
+          <label class="custom-control-label" for="credit">Credit card</label>
+        </div>
+        <div class="custom-control custom-radio">
+          <input id="cash" name="paymentMethod" type="radio" class="custom-control-input" required>
+          <label class="custom-control-label" for="cash">Cash</label>
+        </div>
+        <div class="custom-control custom-radio">
+          <input id="paypal" name="paymentMethod" type="radio" class="custom-control-input" required>
+          <label class="custom-control-label" for="paypal">Paypal</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <label for="cc-name">Name on card</label>
+          <input type="text" class="form-control" id="cc-name" placeholder="" required>
+          <small class="text-muted">Full name as displayed on card</small>
+          <div class="invalid-feedback">
+            Name on card is required
+          </div>
+        </div>
+        <div class="col-md-6 mb-3">
+          <label for="cc-number">Credit card number</label>
+          <input type="text" class="form-control" id="cc-number" placeholder="" required>
+          <div class="invalid-feedback">
+            Credit card number is required
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <label for="cc-expiration">Expiration</label>
+          <input type="text" class="form-control" id="cc-expiration" placeholder="" required>
+          <div class="invalid-feedback">
+            Expiration date required
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <label for="cc-expiration">CVV</label>
+          <input type="text" class="form-control" id="cc-cvv" placeholder="" required>
+          <div class="invalid-feedback">
+            Security code required
+          </div>
+        </div>
+      </div>
+      <hr class="mb-4">
+      <button class="btn btn-primary btn-lg btn-block" type="submit">Continue to checkout</button>
+    @csrf
+    </form>
+  </div>
+</div>
 @endsection
-
-
