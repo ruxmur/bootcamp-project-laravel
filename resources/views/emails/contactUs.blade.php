b6cd6434f86389bce3c5ac45514ce43adc9013da
ruxmur <r.murtacaeva@developmentaid.it>
2022-01-13 19:49:00 +0200

    Changed the elements from <body> in contactUs.

--- resources/views/emails/contactUs.blade.php
+++ resources/views/emails/contactUs.blade.php
@@ -1,19 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
- </head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
 <body>
   <p>User requested attention:</p>
   <p>User firstName: {{ $firstName }}</p>
   <p>User lastName: {{ $lastName }}</p>
   <p>User email: {{ $email }}</p>
   <p>User tel: {{ $tel }}</p>
-  <p>User sex: {{ $sex }}</p>
-  <p>User membership: {{ $membership }}</p>
-  <p>User sport: {{ $sport }}</p>
-  <p>User coach: {{ $coach }}</p>
+  <p>Sex interested in: {{ implode(',', $sex ?? []) }}</p>
+  <p>Membership interested in: {{ implode(',', $membership ?? []) }}</p>
+  <p>Sport interested in: {{ implode(',', $sport ?? []) }}</p>
+  <p>Coach interested in: {{ implode(',', $coach ?? []) }}</p>
   <p>User messege:</p>
   <p>{{ $messageText }}</p>
 
 </body>
+
 </html>
