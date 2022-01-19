338421cd48f1f654752be35b861b598eb5b4887c
ruxmur <r.murtacaeva@developmentaid.it>
2022-01-19 20:17:30 +0200

    modify some issue in Controllers

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,7 +14,7 @@ class BlogController extends Controller
     $sort = $request['sort'] ?? 'ASC';
 
     $categories = BlogCategory::all();
-    $category = $request['category'] ?? $categories->first()->id;
+    $category = $request['category'] ?? $categories->first();
 
     $articles = Article::orderby('created_at', $sort)->paginate(7);
     $articles->appends(['sort' => $sort]);
