95a5f1c03f34b8c345ace70376b0508f00bb7de6
ruxmur <r.murtacaeva@developmentaid.it>
2022-01-21 10:59:32 +0200

    Remode Debug and Production Loggers from LogActivityMiddleware

--- app/Http/Middleware/LogActivityMiddleware.php
+++ app/Http/Middleware/LogActivityMiddleware.php
@@ -3,27 +3,23 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use App\Services\DebugRequestActivityLogger;
 use App\Services\DummyRequestActivityLogger;
-use App\Services\ProductionRequestActivityLogger;
 use Illuminate\Http\Request;
 
 
 class LogActivityMiddleware
 {
     private DummyRequestActivityLogger $logger;
-    private DebugRequestActivityLogger $debug;
-    private ProductionRequestActivityLogger $production;
+   
 
     /**
      * @param DummyRequestActivityLogger $logger
      */
 
-    public function __contruct(DummyRequestActivityLogger $logger, DebugRequestActivityLogger $debug, ProductionRequestActivityLogger $production)
+    public function __contruct(DummyRequestActivityLogger $logger)
     {
         $this->logger = $logger;
-        $this->debug = $debug;
-        $this->production = $production;
+        
 
     }
 
@@ -36,8 +32,7 @@ class LogActivityMiddleware
     public function handle($request, Closure $next, ?string $type = null)
     {
         $this->logger->logRequest($request, $type ?? 'unknown page');
-        $this->debug->logRequest($request, $type ?? 'unknown page');
-        $this->production->logRequest($request, $type ?? 'unknown page');
+        
 
         return $next($request);
     }
