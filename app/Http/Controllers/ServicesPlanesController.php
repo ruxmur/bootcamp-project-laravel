a06ba74cbd1115030b5c5b601e72bdc82aa7994b
ruxmur <r.murtacaeva@developmentaid.it>
2022-02-22 19:27:41 +0200

    working on bags from services page

--- app/Http/Controllers/ServicesPlanesController.php
+++ app/Http/Controllers/ServicesPlanesController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Membership;
+use App\Services\ModelLogger;
+
 
 class ServicesPlanesController extends Controller
 {
-    public function services()
+    public function showService(/*$membershipId,*/ Request $request/*, ModelLogger $logger*/)
     {
-        return view('home.services');
     }
+
+
+    // public function showList(/*$membershipId,*/ Request $request/*, ModelLogger $logger*/)
+    // {
+    //     $services = Membership::with('name', 'type', 'duration')->get();
+        
+    //     $service = $services::findOrFail($membershipId);
+      
+    //     // $logger->logModel($request->user(), $service); 
+   
+    //     return view('home.services', ['services' => $services]);
+    // }
+    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -12,18 +12,24 @@ use App\Http\Controllers\ArticleController;
 
 
 Route::get('/',[HomeController::class, 'index'])->name('home');
-Route::get('/services',[ServicesPlanesController::class, 'services'])->name('services')
-->middleware('log.activity:services');
+Route::get('/services', [ServicesPlanesController::class, 'showList'])
+->name('services')
+// ->middleware('log.activity:services')
+;
 Route::get('/team',[TeamController::class, 'team'])->name('team');
 Route::get('/about',[AboutController::class, 'about'])->name('about');
 
 Route::get('/contacts',[ContactUsController::class, 'contact'])->name('contacts');
 Route::post('/contactUs',[ContactUsController::class, 'send'])->name('contactUs.send')
-->middleware('log.activity:sendContactUs');
+->middleware('log.activity:sendContactUs')
+;
 
 Route::get('/blog',[BlogController::class, 'blog'])->name('blog');
-Route::get('/blog/article/{id}',[ArticleController::class, 'show'])->name('blogArticle');
+Route::get('/blog/article/create',[ArticleController::class, 'create']);
+Route::get('/blog/article/{id}',[ArticleController::class, 'show'])->name('article');
 
 Route::get('/api/articles/most-popular',[ArticleApiController::class, 'readMostPopular']);
 Route::get('/api/articles',[ArticleApiController::class, 'readAll']);
 Route::get('/api/articles/{id}',[ArticleApiController::class, 'readOne']);
+Route::delete('/api/articles/{id}',[ArticleApiController::class, 'deleteArticle']);
+Route::post('/api/articles',[ArticleApiController::class, 'createArticle']);
