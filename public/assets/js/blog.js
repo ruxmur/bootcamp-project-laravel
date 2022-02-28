791d7c66c51b0d5ebdaff31ee4f41759ea1949d5
developer@developmentaid.org <developer@developmentaid.org>
2022-02-28 10:45:55 +0200

    updated the style

--- public/assets/js/blog.js
+++ public/assets/js/blog.js
@@ -164,7 +164,37 @@ var __webpack_exports__ = {};
   \***********************************/
 __webpack_require__(/*! ./most-popular */ "./resources/blog/js/most-popular.js");
 
-__webpack_require__(/*! ./create-article */ "./resources/blog/js/create-article.js");
+__webpack_require__(/*! ./create-article */ "./resources/blog/js/create-article.js"); // const popularNewsTemplate = document.querySelector("[popular-artcles-template]");
+// const listOfArticles = document.querySelector("[articles-list]");
+// axios.get('/api/articles/most-popular')
+//     .then(({ data }) => {
+//         data.forEach(article => {
+//             const articleOnPage = popularNewsTemplate.content.cloneNode(true).children[0];
+//             let title = articleOnPage.querySelector(".card-title");
+//             title.textContent = article.title;
+//             title.style.cursor = "pointer";
+//             title.addEventListener("click", () =>{
+//                 location.href = `http://localhost:880/blog/article/${article.id}`;
+//             })
+//             articleOnPage.querySelector(".card-text").textContent = article.excerpt;
+//             let image = articleOnPage.querySelector(".bg-image");
+//             image.style.backgroundImage = `url(http://localhost:880/storage/${article.image_url})`;
+//             image.style.backgroundSize = "contain";
+//             image.style.backgroundPosition = "center";
+//             image.style.backgroundRepeat = "no-repeat";
+//
+//             if (!article.view_count) {
+//                 articleOnPage.querySelector(".badge").remove();
+//             }else{
+//                 articleOnPage.querySelector(".badge").innerHTML = `
+//                 ${article.view_count}
+//                 <span class="visually-hidden">unread messages</span>`;
+//             }
+//
+//             listOfArticles.append(articleOnPage);
+//
+//         });
+//     })
 })();
 
 /******/ })()
--- public/assets/js/most-popular.js
+++ public/assets/js/most-popular.js
@@ -1,25 +1,32 @@
 /******/ (() => { // webpackBootstrap
 var __webpack_exports__ = {};
-/*!**************************************!*\
-  !*** ./resources/js/most-popular.js ***!
-  \**************************************/
-var popularArticleTemplate = document.querySelector('[popular-article-template');
-var listOfArticles = document.querySelector('[articles-list]');
-axios.get('/api/articles/most-popular').then(function (_ref) {
-  var data = _ref.data;
-  data.forEach(function (articleItem) {
-    var article = popularArticleTemplate.textContent.cloneNode(true).children[0];
-    article.querySelector('[data-title').textContent = articleItem.title;
-    article.querySelector('[data-excerpt').textContent = articleItem.excerpt;
-    var counterElement = article.querySelector('[data-views-counter]');
-    counterElement.innerHtml = "\n        ".concat(articleItem.view_count, " \n        <span class=\"visually-hidden\">unread message</span>\n        ");
+/*!*******************************************!*\
+  !*** ./resources/blog/js/most-popular.js ***!
+  \*******************************************/
+if (document.querySelector('[popular-article-template]')) {
+  var popularArticleTemplate = document.querySelector('[popular-article-template]');
+  var listOfArticles = document.querySelector('[articles-list]');
+  axios.get('/api/articles/most-popular').then(function (_ref) {
+    var data = _ref.data;
+    console.log(data);
+    data.forEach(function (articleItem) {
+      var article = popularArticleTemplate.content.cloneNode(true).children[0];
+      article.querySelector('[data-title]').textContent = articleItem.title;
+      article.querySelector('[data-title]').addEventListener('click', function () {
+        location.href = 'blog/article/' + articleItem.id;
+      }, false);
+      article.querySelector('[data-excerpt]').textContent = articleItem.excerpt;
+      article.children[0].style.backgroundImage = "url(storage/" + articleItem.image + ")";
+      var counterElement = article.querySelector('[data-views-counter]');
+      counterElement.innerHTML = "\n            ".concat(articleItem.view_count, "\n            <span class=\"visually-hidden\">unread messages</span>");
 
-    if (!articleItem.view_count) {
-      counterElement.hidden = true;
-    }
+      if (!articleItem.view_count) {
+        counterElement.hidden = true;
+      }
 
-    listOfArticles.append(article);
+      listOfArticles.append(article);
+    });
   });
-});
+}
 /******/ })()
 ;
